674a43a3faef6306275721fbfcd1b5d5ee48ee12
Brad <brad.zhao27@outlook.sg>
2026-03-07 11:54:51 +0800

    restart

--- include/boruvka.h
+++
@@ -1,8 +0,0 @@
-#ifndef BORUVKA_H
-#define BORUVKA_H
-
-#include "graph.h"
-
-int boruvka_mst(const Graph& g);
-
-#endif
--- src/boruvka.cpp
+++
@@ -1,6 +0,0 @@
-#include <boruvka.h>
-
-int boruvka_mst(const Graph& g) {
-    // implementation
-    return 0;
-}
